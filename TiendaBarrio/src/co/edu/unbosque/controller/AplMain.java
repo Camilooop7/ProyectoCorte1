5d46dab79a12669637bd3c4ff71ae99870468043
HatosJeisson <jeissoneliasherresil@gmail.com>
2025-08-13 19:02:30 -0500

    prueba
    aaaaa

--- TiendaBarrio/src/co/edu/unbosque/controller/AplMain.java
+++ TiendaBarrio/src/co/edu/unbosque/controller/AplMain.java
@@ -1,9 +1,11 @@
 package co.edu.unbosque.controller;
 
+import java.io.IOException;
+
 public class AplMain {
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Controller c = new Controller();
 		c.run();
 	}
--- TiendaBarrio/src/co/edu/unbosque/view/VentanaPrincipal.java
+++ TiendaBarrio/src/co/edu/unbosque/view/VentanaPrincipal.java
@@ -1,15 +1,51 @@
 package co.edu.unbosque.view;
 
-import javax.swing.JFrame;
+import java.io.IOException;
 
+import javax.swing.JFrame;
+/**
+ * Clase la cual es llamada como Ventana y extiende JPanel lo que permite
+ * agregar botones y o componentes de interfaz grafica. y creación de las
+ * variables con su nombre privadas.
+ */
 public class VentanaPrincipal extends JFrame {
 
-	public VentanaPrincipal() {
+	private PanelPrincipal pp;
+	private PanelSesion ps;
+
+	/**
+	 * Constructor del panel donde se ejecuta la logica en general de cada parametro
+	 * que se encuentra en la ventana. además se declara la excepción de
+	 * IOexception.
+	 */
+	public VentanaPrincipal() throws IOException {
 		setBounds(10, 10, 1290, 750);
 		setTitle("Tienda de Barrio");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setLayout(null);
-
+		
+		pp = new PanelPrincipal();
+		ps = new PanelSesion();
+		
+		add(pp).setVisible(false);
+		add(ps).setVisible(true);
 	}
+
+	public PanelPrincipal getPp() {
+		return pp;
+	}
+
+	public void setPp(PanelPrincipal pp) {
+		this.pp = pp;
+	}
+
+	public PanelSesion getPs() {
+		return ps;
+	}
+
+	public void setPs(PanelSesion ps) {
+		this.ps = ps;
+	}
+
 }
